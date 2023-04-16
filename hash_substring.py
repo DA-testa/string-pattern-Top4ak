d44a69808b37d906a0b7b87228e32a36a8432d1c
Top4ak <120311720+Top4ak@users.noreply.github.com>
2023-04-17 01:33:00 +0300

    221RDB014

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,57 @@
-# python3
+# 13.grupa 221RDB014
 
-def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+class RabinKarpSearch:
 
-def print_occurrences(output):
-    # this function should control output, it doesn't need any return
-    print(' '.join(map(str, output)))
+    def __init__(self, ag=256, q=101):
+        self.ag = ag
+        self.q = q
 
-def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    def read_input(self):
+        ievade = input()
 
-    # and return an iterable variable
-    return [0]
+        if "F" in ievade:
+            Fails = 'tests/06'
+            with open(Fails, 'r') as f:
+                pattern = f.readline().rstrip()
+                text = f.readline().rstrip()
+
+        elif "I" in ievade:
+            pattern = input().rstrip()
+            text = input().rstrip()
+
+        return pattern, text
+
+    def print_occurrences(self, output):
+        print(' '.join(str(x) for x in output))
+
+    def get_occurrences(self, pattern, text):
+        pg = len(pattern)
+        tg = len(text)
+
+        indeks = []
+        p_hash = 0
+        t_hash = 0
+        h = 1
+
+        for i in range(pg - 1):
+            h = (h * self.ag) % self.q
+
+        for i in range(pg):
+            p_hash = (self.ag * p_hash + ord(pattern[i])) % self.q
+            t_hash = (self.ag * t_hash + ord(text[i])) % self.q
+
+        for i in range(tg - pg + 1):
+            if p_hash == t_hash:
+                if pattern == text[i:i + pg]:
+                    indeks.append(i)
+
+            if i < tg - pg:
+                t_hash = (self.ag * (t_hash - ord(text[i]) * h) + ord(text[i + pg])) % self.q
+                t_hash = (t_hash + self.q) % self.q
+
+        return indeks
 
 
-# this part launches the functions
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
-
+    rabin_karp_search = RabinKarpSearch()
+    rabin_karp_search.print_occurrences(rabin_karp_search.get_occurrences(*rabin_karp_search.read_input()))
